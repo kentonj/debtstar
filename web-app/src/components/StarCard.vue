203361dbfcf1a48c408e8941be544db72e3826bb
Paige Kelley <oryan504@gmail.com>
2019-10-04 09:30:26 -0700

    Math

--- web-app/src/components/StarCard.vue
+++ web-app/src/components/StarCard.vue
@@ -10,28 +10,28 @@
       .column.is-narrow(v-else)
         .box.main-number-debt
           span.main-number-font ${{ totalEarnedOverTime }}
-          P Value of ${{ dollarsInvested}}
-          p with savings over {{this.term}} months
+          P Debt reduced by ${{ dollarsInvested}}
+          p over {{this.term}} months
       .column
         h2 Current
         .label.has-text-grey-dark Current Value:
-          span.information-text &nbsp; ${{ item.current_balance }}
+          span.information-text &nbsp; ${{ currentBalanceFormatted }}
         .label.has-text-grey-dark Interest:
           span.information-text &nbsp; {{ item.interest * 100 }}%
         .label.has-text-grey-dark(v-if="item.minimum_payment") Monthly Payment:
-          span.information-text &nbsp; ${{ item.minimum_payment }}
-        .label.has-text-grey-dark Monthly Interest:
-          span.information-text &nbsp; ${{ currentMonthlyInterest }}
+          span.information-text &nbsp; ${{ formattedPayment }}
+        //- .label.has-text-grey-dark Interest from Payment:
+        //-   span.information-text &nbsp; ${{ currentMonthlyInterest }}
       .column 
         h2 Projection In {{ this.term }} Months
         .label.has-text-grey-dark(v-if="item.is_debt") Value Remaining:
-          span.information-text &nbsp; ${{ projectedValue }}
+          span.information-text &nbsp; ${{ futureValueWithPayments }}
         .label.has-text-grey-dark Interest:
           span.information-text &nbsp; {{ item.interest * 100 }}%
         .label.has-text-grey-dark(v-if="item.minimum_payment") Monthly Payment:
-          span.information-text &nbsp; ${{ item.minimum_payment }}
-        .label.has-text-grey-dark Monthly Interest:
-          span.information-text &nbsp; ${{ futureMonthlyInterest }}
+          span.information-text &nbsp; ${{ formattedPayment }}
+        //- .label.has-text-grey-dark Interest from Payment:
+        //-   span.information-text &nbsp; ${{ futureMonthlyInterest }}
       .column.is-narrow
         b-button.theme-dark-blue.has-text-white(
           @click="makePayment"
@@ -60,14 +60,44 @@ export default {
     };
   },
   computed: {
+    currentBalanceFormatted() {
+      return Number(this.item.current_balance).toFixed(2);
+    },
+    formattedPayment() {
+      return Number(this.item.minimum_payment).toFixed(2);
+    },
+    numberCurrentBalance() {
+      return Number(this.item.current_balance);
+    },
+    numberTerm() {
+      return Number(this.term);
+    },
+    numberDollarInvested() {
+      return this.dollarsInvested;
+    },
+    numberInterest() {
+      return Number(this.item.interest);
+    },
+    numberMinPayment() {
+      return Number(this.item.minimum_payment);
+    },
     totalEarnedOverTime() {
-      const fv = this.dollarsInvested * (1 + this.item.interest / 12) ** (this.term);
+      const fv = this.dollarsInvested * (1 + this.item.interest / 12) ** (this.numberTerm);
       const val = Number(fv);
       const formatted_val = val.toFixed(2);
       return formatted_val;
     },
+    futureValueWithPayments() {
+      const original_value_fv = (this.numberCurrentBalance * ((1 + this.numberInterest / 12) ** this.numberTerm));
+      const monthly_payment_fv = ((((1 + this.numberInterest / 12) ** this.term) -1 ) / (this.numberInterest/12)) * this.numberMinPayment;
+      const future_val_with_payments = original_value_fv - monthly_payment_fv;
+      const dollars_invested_fv = (this.numberDollarInvested * ((1 + this.numberInterest / 12) ** this.numberTerm));
+      const future_val_with_extra_dollars = future_val_with_payments - dollars_invested_fv;
+      const formatFV = Number(future_val_with_extra_dollars).toFixed(2);
+      return formatFV;
+    },
     projectedValue() {
-      const val = this.dollarsInvested + (this.term * this.item.minimum_payment);
+      const val = this.totalEarnedOverTime + (this.term * this.item.minimum_payment);
       const val2 = this.item.current_balance - val;
       return Number(val2).toFixed(2);
     },
